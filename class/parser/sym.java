cdf20e61c8283ca9bf595a397252a9f109b1186b
Optimus-T <luisgmorales@ufm.edu>
2024-08-29 15:40:18 -0600

    Added Sym
    added sym.java file

---
+++ class/parser/sym.java
@@ -0,0 +1,44 @@
+package parser;
+
+public class sym {
+    public static final int BOOLEAN = 31;  // Palabra clave 'boolean'
+    public static final int COMMA = 18;  // Coma ','
+    public static final int DIVIDE = 21;  // División '/'
+    public static final int ELSE = 25;  // Palabra clave 'else'
+    public static final int EOF = 0;  // Fin de archivo (End Of File)
+    public static final int EQUALS = 37;  // Igual '='
+    public static final int EQUALS_EQUALS = 8;  // Igual a '=='
+    public static final int FALSE = 27;  // Palabra clave 'false'
+    public static final int FLOAT = 28;  // Palabra clave 'float'
+    public static final int FLOATLIT = 24;  // Literal flotante
+    public static final int FOR = 35;  // Palabra clave 'for'
+    public static final int GREATER_THAN = 10;  // Mayor que '>'
+    public static final int GREATER_THAN_EQUALS = 11;  // Mayor o igual que '>='
+    public static final int ID = 2;  // Identificador (ID)
+    public static final int IDENTIFIER = 38;  // Identificador (IDENTIFIER)
+    public static final int IF = 3;  // Palabra clave 'if'
+    public static final int INT = 1;  // Palabra clave 'int'
+    public static final int INTLIT = 22;  // Literal entero
+    public static final int LBRACE = 16;  // Llave izquierda '{'
+    public static final int LBRACKET = 14;  // Corchete izquierdo '['
+    public static final int LESS_THAN = 36;  // Menor que '<'
+    public static final int LESS_THAN_EQUALS = 7;  // Menor o igual que '<='
+    public static final int LPAREN = 12;  // Paréntesis izquierdo '('
+    public static final int MINUS = 19;  // Menos '-'
+    public static final int MULTIPLY = 20;  // Multiplicación '*'
+    public static final int NOT_EQUALS = 9;  // Diferente de '!='
+    public static final int NUM = 5;  // Número (NUM)
+    public static final int PLUS = 6;  // Más '+'
+    public static final int PRIVATE = 32;  // Palabra clave 'private'
+    public static final int PROTECTED = 33;  // Palabra clave 'protected'
+    public static final int PUBLIC = 30;  // Palabra clave 'public'
+    public static final int RBRACE = 17;  // Llave derecha '}'
+    public static final int RBRACKET = 15;  // Corchete derecho ']'
+    public static final int RETURN = 4;  // Palabra clave 'return'
+    public static final int RPAREN = 13;  // Paréntesis derecho ')'
+    public static final int SEMICOLON = 39;  // Punto y coma ';'
+    public static final int STRINGLIT = 23;  // Literal de cadena
+    public static final int TRUE = 26;  // Palabra clave 'true'
+    public static final int VOID = 34;  // Palabra clave 'void'
+    public static final int WHILE = 29;  // Palabra clave 'while'
+}
